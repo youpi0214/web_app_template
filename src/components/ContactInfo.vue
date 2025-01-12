bc2e1aa0e7ce0e5f1cc33b285b9fa7e301054401
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-01-12 00:00:11 -0500

    correction

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -3,20 +3,20 @@
     <div>
       <h3 class="text-xl font-bold text-white">Adresse</h3>
       <p class="text-white">
-        129 Rue des Oeillets, Lévis, QC G6V 7S9
+        400 Scott St, St Catharines, ON L3M 3W4
       </p>
     </div>
     <div>
       <h3 class="text-xl font-bold text-white">Téléphone</h3>
       <p class="text-white">
-        <a href="tel:+14389854612" class="hover:underline hover:text-accent">+1 438 985 4612</a>
+        <a href="tel:+199999999" class="hover:underline hover:text-accent">+1 999 999 9999</a>
       </p>
     </div>
     <div>
       <h3 class="text-xl font-bold text-white">Email</h3>
       <p class="text-white">
-        <a href="mailto:services@solidium.ca" class="text-white hover:underline hover:text-accent">
-          services@solidium.ca
+        <a href="mailto:email@email.ca" class="text-white hover:underline hover:text-accent">
+          email@email.ca
         </a>
       </p>
     </div>
