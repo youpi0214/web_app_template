e48a1918443ec51af292d43a8703f099ff8864e7
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-01-11 23:16:25 -0500

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx,vue}",
+    "./public/**/*.html", // Include HTML files in the public folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
